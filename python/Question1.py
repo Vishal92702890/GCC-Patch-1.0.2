f794d67643596c8e33789abe318becbd18e1c0bd
Vishal92702890 <49938587+Vishal92702890@users.noreply.github.com>
2019-10-08 17:21:18 +0530

    Delete Question1.py

--- python/Question1.py
+++
@@ -1,5 +0,0 @@
-# modify this function, and create other functions below as you wish
-def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
-    # modify and then return the variable below
-    answer = -5
-    return answer
